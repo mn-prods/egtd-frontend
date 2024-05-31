1c52b4dd16d082db46e7619850391ef01dadf79b
marnec <marnec@ryzenbeast.localdomain>
2024-05-31 19:29:04 +0200

    completely remove ionic

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { enableProdMode, isDevMode } from '@angular/core';
 import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, indexedDBLocalPersistence, initializeAuth, provideAuth } from '@angular/fire/auth';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter } from '@angular/router';
+import { RouteReuseStrategy, provideRouter, withHashLocation } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
@@ -16,7 +16,7 @@ bootstrapApplication(AppComponent, {
     providers: [
         provideFirebaseApp(() => initializeApp(environment.firebase)),
         provideAuth(() => getAuth()),
-        provideRouter(routes),
+        provideRouter(routes, withHashLocation()),
 
     ],
 });
